d293e175c989a03cecd36828bc465504cad4e184
UveGG <uveuve73@mail.ru>
2019-04-28 00:21:04 +0300

    Added more comfortable conditions for user

--- AlisaFile.py
+++ AlisaFile.py
@@ -33,6 +33,18 @@ def init_route(app, db):
         user_answer = req['request']['original_utterance'].lower()
 
         if req['session']['new']:
+
+            res['response']['buttons'] = [
+                {
+                    'title': 'Помощь',
+                    'hide': False
+                },
+                {
+                    'title': 'На этом все',
+                    'hide': False
+                }
+            ]
+
             alisa_answer = 'Привет! Это моё умение предназначено для помощи школьнику и его родителям. ' \
                            'Я могу подсказать последнее домашнее задание или вывести таблицу из отчетов,' \
                            ' сказать средний балл или перечислить оценки по предмету, подвести итоги' \
@@ -55,7 +67,7 @@ def init_route(app, db):
 
                 alisa_answer += 'Но сначала нужно авторизироваться, чтобы мне было с чем работать. ' \
                                 'Для авторизации мне нужен номер Вашего региона, населённый пункт, школа, ' \
-                                'а также Ваш логин и пароль. Сейчас я прошу Вас написать номер региона или его название (отдельным сообщением).'
+                                'а также Ваш логин и пароль. Сейчас я прошу Вас написать номер региона (отдельным сообщением).'
             else:
                 user = User.query.filter_by(user_id=user_id).first()
 
@@ -65,7 +77,7 @@ def init_route(app, db):
                 sessionStorage[user_id]['school'] = user.school_id
                 sessionStorage[user_id]['login'] = user.login
                 sessionStorage[user_id]['password'] = user.password
-                sessionStorage[user_id]['connection'] = ['connected', 'old_account', 'waiting']
+                sessionStorage[user_id]['connection'] = [False, 'old_account', 'waiting']
                 sessionStorage[user_id]['authorisation'] = False
 
                 alisa_answer += ' А сейчас я обновляю базу данных. Подождите чуть-чуть, пожалуйста!'
@@ -84,10 +96,69 @@ def init_route(app, db):
 
         # noinspection PySimplifyBooleanCheck
         if sessionStorage[user_id]['authorisation']:
+
+            res['response']['buttons'] = [
+                {
+                    'title': 'Помощь',
+                    'hide': False
+                },
+                {
+                    'title': 'Что ты можешь?',
+                    'hide': False
+                },
+                {
+                    'title': 'Отмена',
+                    'hide': False
+                }
+            ]
+
+            if 'отмена' in user_answer:
+                res['response']['buttons'] = [
+                    {
+                        'title': 'Помощь',
+                        'hide': False
+                    },
+                    {
+                        'title': 'Что ты можешь?',
+                        'hide': False
+                    },
+                    {
+                        'title': 'На этом все',
+                        'hide': False
+                    }
+                ]
+
+                alisa_answer = 'Хорошо, давайте начнем вход в аккаунт заново. Если вы хотите покинуть умение напишите "На этом все".'
+                res['response']['text'] = alisa_answer
+
+                sessionStorage[user_id]['region'] = None
+                sessionStorage[user_id]['city'] = None
+                sessionStorage[user_id]['school'] = None
+                sessionStorage[user_id]['login'] = None
+                sessionStorage[user_id]['password'] = None
+
+                return
+
             if sessionStorage[user_id]['region'] is None:
                 if 'помощь' in user_answer or 'помоги' in user_answer:
                     alisa_answer = 'Напишите номер Вашего региона для входа в "Сетевой Город".'
                     res['response']['text'] = alisa_answer
+
+                    res['response']['buttons'] = [
+                        {
+                            'title': 'Помощь',
+                            'hide': False
+                        },
+                        {
+                            'title': 'Что ты можешь?',
+                            'hide': False
+                        },
+                        {
+                            'title': 'На этом все',
+                            'hide': False
+                        }
+                    ]
+
                     return
                 if 'что ты можешь' in user_answer:
                     alisa_answer = 'Я могу подсказать последнее домашнее задание или вывести таблицу из отчетов,' \
@@ -114,10 +185,6 @@ def init_route(app, db):
                     sessionStorage[user_id]['region'] = user_answer
                     alisa_answer = 'Отлично! Теперь выберите название Вашего населённого пункта!'  # Достать названия всех
                     res['response']['text'] = alisa_answer
-                    res['response']['buttons'] = [{
-                        'title': 'Помощь',
-                        'hide': False
-                    }]
 
                     cities = City.query.filter_by(region_id=sessionStorage[user_id]['region'])
                     for city in cities:
@@ -125,6 +192,7 @@ def init_route(app, db):
                             'title': city.name,
                             'hide': False
                         })
+
                     return
 
             if sessionStorage[user_id]['city'] is None:
@@ -132,11 +200,6 @@ def init_route(app, db):
                     alisa_answer = 'Выберите Ваш населенный пункт для входа в "Сетевой Город".'
                     res['response']['text'] = alisa_answer
 
-                    res['response']['buttons'] = [{
-                        'title': 'Помощь',
-                        'hide': False
-                    }]
-
                     cities = City.query.filter_by(region_id=sessionStorage[user_id]['region'])
                     for city in cities:
                         res['response']['buttons'].append({
@@ -145,6 +208,7 @@ def init_route(app, db):
                         })
 
                     return
+
                 if 'что ты можешь' in user_answer:
                     alisa_answer = 'Я могу подсказать последнее домашнее задание или вывести таблицу из отчетов,' \
                                    ' сказать средний балл или перечислить оценки по предмету, подвести итоги' \
@@ -155,11 +219,6 @@ def init_route(app, db):
                                    ' "Помощь" или "Что ты можешь?". Чтобы завершить работу с "Сетевым городом" напишите "на этом все".' \
                                    ' А сейчас выберите Ваш населенный пункт.'
                     res['response']['text'] = alisa_answer
-
-                    res['response']['buttons'] = [{
-                        'title': 'Помощь',
-                        'hide': False
-                    }]
 
                     cities = City.query.filter_by(region_id=sessionStorage[user_id]['region'])
                     for city in cities:
@@ -173,10 +232,6 @@ def init_route(app, db):
                 if not City.query.filter_by(region_id=sessionStorage[user_id]['region'], name=user_answer).first():
                     alisa_answer = 'Что это за место? Я о нем не слышала!'
                     res['response']['text'] = alisa_answer
-                    res['response']['buttons'] = [{
-                        'title': 'Помощь',
-                        'hide': False
-                    }]
 
                     cities = City.query.filter_by(region_id=sessionStorage[user_id]['region'])
                     for city in cities:
@@ -191,10 +246,19 @@ def init_route(app, db):
                     alisa_answer = 'Отлично! Теперь выберите название Вашей образовательной организации!'  # Достать названия всех
                     res['response']['text'] = alisa_answer
 
-                    res['response']['buttons'] = [{
-                        'title': 'Помощь',
-                        'hide': False
-                    }]
+                    schools = School.query.filter_by(city_id=sessionStorage[user_id]['city'])
+                    for school in schools:
+                        res['response']['buttons'].append({
+                            'title': school.name,
+                            'hide': False
+                        })
+
+                    return
+
+            if sessionStorage[user_id]['school'] is None:
+                if 'помощь' in user_answer or 'помоги' in user_answer:
+                    alisa_answer = 'Выберите Вашу образовательную организацию для входа в "Сетевой Город".'
+                    res['response']['text'] = alisa_answer
 
                     schools = School.query.filter_by(city_id=sessionStorage[user_id]['city'])
                     for school in schools:
@@ -202,12 +266,7 @@ def init_route(app, db):
                             'title': school.name,
                             'hide': False
                         })
-                    return
 
-            if sessionStorage[user_id]['school'] is None:
-                if 'помощь' in user_answer or 'помоги' in user_answer:
-                    alisa_answer = 'Выберите Вашу образовательную организацию для входа в "Сетевой Город".'
-                    res['response']['text'] = alisa_answer
                     return
                 if 'что ты можешь' in user_answer:
                     alisa_answer = 'Я могу подсказать последнее домашнее задание или вывести таблицу из отчетов,' \
@@ -219,15 +278,19 @@ def init_route(app, db):
                                    ' "Помощь" или "Что ты можешь?". Чтобы завершить работу с "Сетевым городом" напишите "на этом все".' \
                                    ' А сейчас выберите Вашу образовательную организацию.'
                     res['response']['text'] = alisa_answer
+
+                    schools = School.query.filter_by(city_id=sessionStorage[user_id]['city'])
+                    for school in schools:
+                        res['response']['buttons'].append({
+                            'title': school.name,
+                            'hide': False
+                        })
+
                     return
 
                 if not School.query.filter_by(city_id=sessionStorage[user_id]['city'], name=user_answer).first():
                     alisa_answer = 'Что это за место? Я о нем не слышала!'
                     res['response']['text'] = alisa_answer
-                    res['response']['buttons'] = [{
-                        'title': 'Помощь',
-                        'hide': False
-                    }]
 
                     schools = School.query.filter_by(city_id=sessionStorage[user_id]['city'])
                     for school in schools:
@@ -247,6 +310,7 @@ def init_route(app, db):
                 if 'помощь' in user_answer or 'помоги' in user_answer:
                     alisa_answer = 'Напишите Ваш логин для входа в "Сетевой Город".'
                     res['response']['text'] = alisa_answer
+
                     return
 
                 if 'что ты можешь' in user_answer:
@@ -260,12 +324,14 @@ def init_route(app, db):
                                    ' А сейчас напишите логин от Вашего аккаунта в "Сетевом городе".'
 
                     res['response']['text'] = alisa_answer
+
                     return
 
                 sessionStorage[user_id]['login'] = user_answer
 
                 alisa_answer = 'Отлично! А теперь скажите Ваш пароль для входа в "Сетевой город".'
                 res['response']['text'] = alisa_answer
+
                 return
 
             if sessionStorage[user_id]['password'] is None:
@@ -285,6 +351,7 @@ def init_route(app, db):
                                    ' А сейчас напишите пароль от Вашего аккаунта в "Сетевом городе".'
 
                     res['response']['text'] = alisa_answer
+
                     return
 
                 sessionStorage[user_id]['password'] = user_answer
@@ -293,6 +360,25 @@ def init_route(app, db):
                                ' Ваших оценок и заданий.'
                 res['response']['text'] = alisa_answer
                 sessionStorage[user_id]['authorisation'] = False
+
+                res['response']['buttons'] = [
+                    {
+                        'title': 'Подождите',
+                        'hide': True
+                    },
+                    {
+                        'title': 'Ну подождите',
+                        'hide': True
+                    },
+                    {
+                        'title': 'Ну подождите чуть-чуть',
+                        'hide': True
+                    },
+                    {
+                        'title': 'Ну пожалуйста!',
+                        'hide': True
+                    }
+                ]
 
                 data = [
                     sessionStorage[user_id]['login'], sessionStorage[user_id]['password'],
@@ -354,6 +440,18 @@ def init_route(app, db):
             return
 
         if sessionStorage[user_id]['connection'][0] == 'connected':
+
+            res['response']['buttons'] = [
+                {
+                    'title': 'Помощь',
+                    'hide': False
+                },
+                {
+                    'title': 'На этом все',
+                    'hide': False
+                }
+            ]
+
             if 'помощь' in user_answer or 'помоги' in user_answer or 'что ты можешь' in user_answer:
                 alisa_answer = 'Я могу подсказать последнее домашнее задание или вывести таблицу из отчетов,' \
                                ' сказать средний балл или перечислить оценки по предмету, подвести итоги' \
